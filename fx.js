e37821900776766e3e291b4b177805f4a4984db2
강수성 <mercuriver@gmail.com>
2021-04-20 02:00:58 +0900

    feat: L.map, L.filter 작성

--- fx.js
+++ fx.js
@@ -33,3 +33,31 @@ const reduce = curry((f, acc, iter) => {
   }
   return acc;
 });
+
+const range = (l) => {
+  let i = -1;
+  let res = [];
+
+  while (++i < l) {
+    res.push(i);
+  }
+
+  return res;
+};
+
+const L = {};
+L.range = function* (l) {
+  let i = -1;
+  while (++i < l) {
+    yield i;
+  }
+};
+
+const take = curry((l, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(a);
+    if (res.length === l) return res;
+  }
+  return res;
+});
