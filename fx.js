cc4be34b6e52c484a0e0acb23deaf5b1e33d3b75
강수성 <mercuriver@gmail.com>
2021-04-21 01:59:15 +0900

    comp

--- fx.js
+++ fx.js
@@ -111,3 +111,28 @@ const range = (l) => {
 
   return res;
 };
+
+const C = {};
+
+function noop() {}
+
+const catchNoop = ([...arr]) => (
+  arr.forEach((a) => (a instanceof Promise ? a.catch(noop) : a)), arr
+);
+
+// C.reduce = curry((f, acc, iter) => {
+//   const iter2 = catchNoop(iter ? [...iter] : [...acc]);
+//   return iter ? reduce(f, acc, iter2) : reduce(f, iter2);
+// });
+
+C.reduce = curry((f, acc, iter) =>
+  iter ? reduce(f, acc, catchNoop(iter)) : reduce(f, catchNoop(acc))
+);
+
+C.take = curry((l, iter) => take(l, catchNoop(iter)));
+
+C.takeAll = C.take(Infinity);
+
+C.map = curry(pipe(L.map, C.takeAll));
+
+C.filter = curry(pipe(L.filter, C.takeAll));
